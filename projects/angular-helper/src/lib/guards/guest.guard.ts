7bdc4f607bb66b4e2194a90375e11d40df54210f
Leon <leonkamerlinn@gmail.com>
2020-07-06 13:27:33 +0200

    1.4.2

--- projects/angular-helper/src/lib/guards/guest.guard.ts
+++ projects/angular-helper/src/lib/guards/guest.guard.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@angular/core';
 import {
     CanActivate,
     CanActivateChild,
-    CanLoad,
-    Route,
-    UrlSegment,
     ActivatedRouteSnapshot,
     RouterStateSnapshot,
     UrlTree
